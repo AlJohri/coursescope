b7e6efe908c36b30c0bd3b83e2901863d7a64a95
Al Johri <al.johri@gmail.com>
2013-11-12 22:35:50 -0600

    Removed requirements model because it wouldn't work.

--- app/models/course.rb
+++ app/models/course.rb
@@ -6,10 +6,10 @@ class Course < ActiveRecord::Base
   has_and_belongs_to_many :instructors
   has_and_belongs_to_many :classrooms
 
-  has_many :requirements
-  has_many :prerequisites, :through => requirements
+  # has_many :requirements
+  # has_many :prerequisites, :through => requirements
 
-  has_many :inverse_requirements, :class_name => "Requirement", :foreign_key => "prerequisite_id"
-  has_many :inverse_prerequisites, :through => :inverse_requirements, :source => :course
+  # has_many :inverse_requirements, :class_name => "Requirement", :foreign_key => "prerequisite_id"
+  # has_many :inverse_prerequisites, :through => :inverse_requirements, :source => :course
 
 end
--- app/models/requirement.rb
+++
@@ -1,4 +0,0 @@
-class Requirement < ActiveRecord::Base
-  belongs_to :course
-  belongs_to :prerequisite, :class_name => "Course"
-end
